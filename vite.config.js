dc779c72085fa1a21ebb9f28132407345cb0245b
Mapendokamana <127545345+Mapendokamana@users.noreply.github.com>
2025-02-08 15:27:54 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mapendo-3D-portfio-website/',
+  base: '/Mapendo-3D-portfio-website/', 
+  build: {
+    outDir: 'dist', 
+    rollupOptions: {
+      input: 'index.html' 
+    }
+  }
 })
